19bf79ad55f789e4670a94079ff2676734f7627d
aolasz <49680062+aolasz@users.noreply.github.com>
2025-04-11 19:58:15 +0200

    fix(lutris): experimenting

--- home/modules/gaming/lutris.nix
+++ home/modules/gaming/lutris.nix
@@ -1,29 +1,41 @@
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.my.gaming.lutris;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.my.gaming.lutris;
+in {
   options.my.gaming.lutris = {
     enable = lib.mkEnableOption "Lutris";
   };
-
 
   config = lib.mkIf cfg.enable {
     my.gaming.wine.enable = lib.mkForce true;
     my.gaming.mangohud.enable = lib.mkForce true;
 
+    home.sessionVariables = {
+      # General Vulkan setup
+      VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+
+      # For 32-bit applications
+      VK_LAYER_PATH_32 = "${pkgs.pkgsi686Linux.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+
+      # For NVIDIA specifically
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    };
+
     home.packages = with pkgs; [
       lutris
-      vulkan-tools
-      vulkan-loader
-      vulkan-validation-layers
+      # vulkan-tools
+      # vulkan-loader
+      # vulkan-validation-layers
       # GOG-related packages
-      gogdl  # GOG downloader
-      lgogdownloader  # Another GOG downloader
-      innoextract  # Used to extract GOG installers
+      gogdl # GOG downloader
+      lgogdownloader # Another GOG downloader
+      innoextract # Used to extract GOG installers
       p7zip
-      cabextract  # For extracting CAB files (used by some GOG games)
+      cabextract # For extracting CAB files (used by some GOG games)
     ];
   };
 }
--- nixos/configs/box.nix
+++ nixos/configs/box.nix
@@ -3,10 +3,13 @@
 # CPU: AMD Ryzen 7 5800X (16) @ 3.80 GHz
 # GPU: NVIDIA GeForce GTX 1080 Ti [Discrete]
 # Memory: 31.27 GiB ECC
-
-{ inputs, config, pkgs, lib, ... }:
-
 {
+  inputs,
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.nixpkgs.nixosModules.notDetected
     inputs.disko.nixosModules.disko
@@ -23,8 +26,8 @@
     gaming.steam.enable = true;
     network = {
       interfaces = {
-        wired = { wired0 = "a8:a1:59:6a:c5:5d"; };
-        wireless = { wireless0 = "b0:a4:60:34:6f:43"; };
+        wired = {wired0 = "a8:a1:59:6a:c5:5d";};
+        wireless = {wireless0 = "b0:a4:60:34:6f:43";};
       };
       shares.enable = true;
       tailscale.enable = true;
@@ -49,7 +52,7 @@
       ];
       # kernelModules = [ "nvidia" ];
     };
-    blacklistedKernelModules = [ "nouveau" ];
+    blacklistedKernelModules = ["nouveau"];
     kernelModules = [
       "kvm-amd"
       "nvidia"
@@ -74,69 +77,51 @@
     "/persist".neededForBoot = true;
   };
 
-  swapDevices = [ ];
+  swapDevices = [];
 
   hardware = {
-    cpu.amd.updateMicrocode = lib.mkDefault
+    cpu.amd.updateMicrocode =
+      lib.mkDefault
       config.hardware.enableRedistributableFirmware;
     graphics = {
       enable = true;
+      enable32Bit = true; # Critical for 32-bit Vulkan support
       extraPackages = with pkgs; [
-        (buildEnv {
-          name = "nvidia-vaapi-drivers";
-          paths = [
-            vaapiVdpau
-            libvdpau-va-gl
-            nvidia-vaapi-driver
-            #libglvnd
-            #egl-wayland
-            #vulkan-tools
-            #vulkan-loader
-            #vulkan-validation-layers
-          ];
-          pathsToLink = [ "/lib" "/lib32" "/share" ];
-        })
+        libva
+        libvdpau-va-gl
+        # nvidia-vaapi-driver
+        vaapiVdpau
+        vulkan-extension-layer # Add explicit ICD packages
+        vulkan-loader
+        vulkan-tools
+        vulkan-validation-layers
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        libva
+        vulkan-loader
+        vulkan-validation-layers
+        vulkan-tools
       ];
     };
     nvidia = {
       modesetting.enable = true;
       open = false;
-      # Currently nvidia-settings does not compile due to missing vulkan
-      # header files:
       nvidiaSettings = true;
       powerManagement.enable = true;
       powerManagement.finegrained = false;
-      #forceFullCompositionPipeline = true;
-      #nvidiaPersistenced = true;
+      forceFullCompositionPipeline = true;
+      nvidiaPersistenced = true;
       # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix
       package = config.boot.kernelPackages.nvidiaPackages.beta;
-      #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      #  version = "565.57.01";
-      #  sha256_64bit = "sha256-buvpTlheOF6IBPWnQVLfQUiHv4GcwhvZW3Ks0PsYLHo=";
-      #  sha256_aarch64 = "sha256-aDVc3sNTG4O3y+vKW87mw+i9AqXCY29GVqEIUlsvYfE=";
-      #  openSha256 = "sha256-/tM3n9huz1MTE6KKtTCBglBMBGGL/GOHi5ZSUag4zXA=";
-      #  settingsSha256 = "sha256-H7uEe34LdmUFcMcS6bz7sbpYhg9zPCb/5AmZZFTx1QA=";
-      #  persistencedSha256 = "sha256-hdszsACWNqkCh8G4VBNitDT85gk9gJe1BlQ8LdrYIkg=";
-        #version = "560.35.03";
-        #sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
-        #sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
-        #openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
-        #settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
-        #persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
-        #version = "555.58.02";
-        #sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-        #sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
-        #openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-        #settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-        #persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
-      # };  # package
-    };  # nvidia
-  };  # hardware
+    }; # nvidia
+  }; # hardware
 
   networking.hostName = "box";
 
   # Load NVIDIA driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver = {
+    videoDrivers = ["nvidia"];
+  };
 
   services.greetd.enable = false;
 
@@ -145,6 +130,15 @@
   nixpkgs.hostPlatform = "x86_64-linux";
 
   powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
+
+  system.activationScripts.vulkan-links = ''
+    mkdir -p /usr/lib
+    mkdir -p /usr/lib32
+    ln -sfn /run/opengl-driver/lib/libvulkan.so.1 /usr/lib/libvulkan.so.1
+    ln -sfn /run/opengl-driver/lib/libvulkan.so.1 /usr/lib32/libvulkan.so.1
+    ln -sfn ${pkgs.vulkan-loader}/lib/libvulkan.so.1 /usr/lib/libvulkan.so.1
+    ln -sfn ${pkgs.pkgsi686Linux.vulkan-loader}/lib/libvulkan.so.1 /usr/lib32/libvulkan.so.1
+  '';
 
   system.stateVersion = "24.05";
 
@@ -165,7 +159,7 @@
                 mountpoint = "/boot";
                 # Fix world-accessible /boot/loader/random-seed
                 # https://github.com/nix-community/disko/issues/527#issuecomment-1924076948
-                mountOptions = [ "umask=0077" ];
+                mountOptions = ["umask=0077"];
               };
             };
             zfs = {
